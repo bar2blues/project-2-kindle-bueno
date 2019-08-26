b035c92a54d9d7308a5fdcdd6abdfb46ad0341ed
bar2blues <bar2blues@gmail.com>
2019-08-26 18:31:37 -0300

    create class Ebook and  isEqual method

---
+++ kindleBueno.js
@@ -0,0 +1,17 @@
+class EBook {
+  constructor(title, genre, author, cover, read, readDate) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.cover = cover;
+    this.read = false;
+    this.readDate = null;
+  }
+  static isEqual(eBookA, eBookB) {
+    return (
+      eBookA.title === eBookB.title &&
+      eBookA.author === eBookB.author &&
+      eBookA.genre === eBookB.genre
+    );
+  }
+}
