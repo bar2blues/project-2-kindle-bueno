daf1df850fc4b27b09a455d91c39957410b0c110
bar2blues <bar2blues@gmail.com>
2019-08-27 22:49:10 -0300

    create class kindle

---
+++ kindleBueno.js
@@ -0,0 +1,10 @@
+class kindle {
+  constructor(){
+    this readBooks = 0;
+    this.notReadYetBooks = 0;
+    this._current = null;
+    this._next = null;
+    this._last = null;
+    this._library = []; 
+  }
+}
