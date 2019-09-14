bb01301004ac69be539109464054ee3a16b353c4
bar2blues <bar2blues@gmail.com>
2019-09-14 09:49:45 -0300

    separate in module

---
+++ class_kindle.js
@@ -0,0 +1,28 @@
+class Kindle {
+  constructor() {
+    this.readBooks = 0;
+    this.notReadYetBooks = 0;
+    this._current = null;
+    this._next = null;
+    this._last = null;
+    this._library = [];
+  }
+  //ñññ
+  _exists(eBook) {
+    return this._library.some(kindleBook => EBook.isEqual(kindleBook, eBook));
+  }
+
+  add(eBook) {
+    if (this._exists) {
+      console.warn('"${eBook.title}" already exists in library');
+    } else if (this._library.length === 0) {
+      this._current = eBook;
+    } else if (this._library.length === 1) {
+      this._next = eBook;
+    }
+
+    this._library.push(eBook);
+
+    this._updateNotReadYetBooks();
+  }
+}
